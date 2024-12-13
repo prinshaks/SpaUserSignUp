240f877d84c6026eb7a28a490eb1717c660f7d07
prinshaks <166309007+prinshaks@users.noreply.github.com>
2024-12-12 21:22:22 -0600

    Add files via upload

---
+++ Spa-User-Signup/src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,34 @@
+package org.launchcode.controllers;
+
+import jakarta.validation.Valid;
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm(Model model){
+        model.addAttribute(new User());
+        return "user/add";
+    }
+
+    @PostMapping()
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        if(!errors.hasErrors() && user.getPassword().equals(verify)){
+            model.addAttribute("username",user.getUsername());
+        return "user/index";
+    }
+        else {
+            model.addAttribute("error","Passwords do not match");
+            return "user/add";
+        }
+    }
+}
---
+++ Spa-User-Signup/src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,50 @@
+package org.launchcode.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+public class User {
+    @Size(min=5, max=15, message="Username must be 5-15 characters long")
+    private String username;
+
+    @Email(message="Invalid format")
+    private String email;
+
+    @Size(min=6, message="Password must be min 6 characters long")
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(String username, String email, String password) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
