f45546d1ddfa90590f754293cac178d2c36e3424
prinshaks <prinshasudha@gmail.com>
2025-03-22 09:19:47 -0500

    hgh

--- Spa-User-Signup/src/main/java/org/launchcode/controllers/SpaDayController.java
+++ Spa-User-Signup/src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -21,7 +21,7 @@ public class SpaDayController {
 
         Client newClient = new Client(skintype, manipedi);
         newClient.setAppropriateFacials(skintype);
-        model.addAttribute("client" , newClient);
+        model.addAttribute ("client" , newClient);
 
         return "menu";
     }
